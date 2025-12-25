81ac5fbf4ac54e585bd74c0288722daa00a02c48
Nkululeko Mthethwa <mthethwan44@gmail.com>
2025-12-25 11:17:43 +0200

    updated product details

--- cartflow/src/app/components/product-details/product-details.ts
+++ cartflow/src/app/components/product-details/product-details.ts
@@ -1,6 +1,9 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart-service';
+import { Auth } from '../../auth/services/auth';
+import { WishlistService } from '../../services/wishlist-service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,6 +15,8 @@ export class ProductDetails {
 @Input() product: any;
 @Output() close = new EventEmitter(); // just trying this method
 private baseUrl = environment.apiUrl;
+constructor( private authSvc: Auth,
+   private cartSvc: CartService, private wishlistSvc: WishlistService){};
 
 closePopup(){
   this.close.emit();
@@ -19,5 +24,21 @@ closePopup(){
 
   getImageUrl(path: string | undefined): string{
   return path ? `${this.baseUrl}${path}` : "assets/img/img-01.jpeg"
+}
+
+  addToCart(productId: number) {
+  if (!this.authSvc.isLoggedIn) {
+    this.cartSvc.addToLocalCart(productId);
+  } else {
+    this.cartSvc.addToDbCart({ productId, quantity: 1 }).subscribe();
+  }
+}
+
+addToWishLIst(productId: number) {
+  if (!this.authSvc.isLoggedIn) {
+    this.wishlistSvc.addToLocalWishList(productId);
+  } else {
+    this.wishlistSvc.addToDbWishList({ productId, quantity: 1 }).subscribe();
+  }
 }
 }
